9d9995e7ec2bc14b0cb8599f2619000dce10bf68
Cevresel <ekokredital90@gmail.com>
2024-11-24 19:19:52 +0300

    Create main.js

---
+++ styles/styles/scripts/main.js
@@ -0,0 +1,11 @@
+document.querySelector('.info').addEventListener('click', function() {
+    alert('Bilgilendirme sayfasına yönlendirileceksiniz.');
+});
+
+document.querySelector('.data-entry').addEventListener('click', function() {
+    alert('Veri Girişi sayfasına yönlendirileceksiniz.');
+});
+
+document.querySelector('.register').addEventListener('click', function() {
+    alert('Kayıt olma sayfasına yönlendirileceksiniz.');
+});
